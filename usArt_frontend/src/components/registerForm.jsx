e49db8ebfffe91953f33e5cbe9c1ea772070b417
Marcus Vivar <marcospalominovivar@gmail.com>
2022-11-03 19:37:35 +0100

    changed modal

--- usArt_frontend/src/components/registerForm.jsx
+++ usArt_frontend/src/components/registerForm.jsx
@@ -21,22 +21,6 @@ import {
 
 
 function Register() {
-  let navigate = useNavigate();
-
-  /*MODAL*/
-  const [modal, setModal] = useState([]);
-  const parseExceptionModal = useRef();
-
-  useEffect(() => {
-    const modal = new Modal(parseExceptionModal.current, { keyboard: false })
-    setModal(modal)
-
-  }, [])
-  /*MODAL*/
-  const modalChangerHome = (e) => {
-    window.location.assign("http://localhost:3000/home")
-  }
-
 
   const initialValues = { username: "", email: "", password: "", passwordRepeat: "" };
   let initialValue = false;
@@ -66,10 +50,8 @@ function Register() {
   };
 
   useEffect(() => {
-
-
     if (Object.keys(formErrors).length === 0 && isSubmit) {
-      modal.show()
+      window.location.assign("http://localhost:3000/home")
     }
   }, [formErrors]);
   const validate = (values, checkValue) => {
@@ -80,10 +62,10 @@ function Register() {
     var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\.-_!@#\$%\^&\*])(?=.{7,})");
     if (!values.username) {
       errors.username = "Username is required!";
-    }else if(values.username.length < 3){
+    } else if (values.username.length < 3) {
       errors.username = "Username must be 3 characters or more! ";
     }
-    else if(!usernameRegex.test(values.username)){
+    else if (!usernameRegex.test(values.username)) {
       errors.username = "Username must not contain special characters";
     }
     if (!values.email) {
@@ -95,9 +77,9 @@ function Register() {
       errors.password = "Password is required";
     } else if (values.password != values.passwordRepeat) {
       errors.password = "Password it is not the same";
-    }else if (values.password.length < 7) {
+    } else if (values.password.length < 7) {
       errors.password = "Password must be 8 characters or more";
-    } 
+    }
     else if (!strongRegex.test(values.password)) {
       errors.password = "Password must contain an uppercase letter, a number, and a special character";
     }
@@ -113,45 +95,29 @@ function Register() {
     <div>
 
       <MDBContainer style={{ marginTop: "14vmin", paddingBottom: "10vmin" }} className="items-align-center justify-content-center " >
-
-
-
-
         <MDBCard className='text-black m-5 items-align-center shadow' style={{ borderRadius: '25px' }}>
-
-
           <MDBCardBody className='shadow'>
             <a href="/home"><BsFillArrowLeftSquareFill size='30' className='mx-3 my-3 shadow' /></a>
             <MDBRow>
               <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
-
                 <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4" style={{ color: "#001a1a" }}>Sign up.</p>
-
                 <form className="w-50 text-center align-items-center" onSubmit={handleSubmit}>
-
                   <div className="">
-
                     <MDBInput label='Username' name="username" id='form1' type='text' placeholder="Username" className='w-100 shadow-sm ' value={formValues.username} onChange={handleChange} />
-
                   </div>
-
                   <p className='text-danger mb-3'>{formErrors.username}</p>
-
                   <div className="">
                     <MDBInput label='Your Email' id='form2' name="email" type='email' className='shadow-sm' placeholder="Email" value={formValues.email} onChange={handleChange} />
                   </div>
                   <p className='text-danger mb-3'>{formErrors.email}</p>
-
                   <div className="">
                     <MDBInput label='Password' id='form3' type='password' name="password" placeholder="Password" className='shadow-sm' value={formValues.password} onChange={handleChange} />
                   </div>
                   <p className='text-danger mb-3'>{formErrors.password}</p>
-
                   <div className=" ">
                     <MDBInput label='Repeat your password' id='form4' type='password' name="passwordRepeat" placeholder="Password" className='shadow-sm' value={formValues.passwordRepeat} onChange={handleChange} />
                   </div>
                   <p className='text-danger mb-3'>{formErrors.password}</p>
-
                   <div class="form-check  text-center align-items-center justify-content-center">
                     <input class="form-check-input d-inline-block px-1 " name="checkboxTerms" type="checkbox" id="flexCheckDefault" value={formValues.checkboxTerms} onChange={handleCheck} />
                     <label class="form-check-label px-3 " for="flexCheckDefault" >
@@ -159,47 +125,21 @@ function Register() {
                     </label>
                   </div>
                   <p className='text-danger'>{formErrors.check}</p>
-
                   <div class="form-check  text-center align-items-center justify-content-center py-3">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2"/>
-                      <label class="form-check-label px-3 " for="flexCheckDefault2">
-                       I agree recieving emails about usArt services.
-                      </label>
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2" />
+                    <label class="form-check-label px-3 " for="flexCheckDefault2">
+                      I agree recieving emails about usArt services.
+                    </label>
                   </div>
-                  
-
                   <button type="button" onClick={handleSubmit} class="btn btn-primary shadow mb-5 mt-3">Register</button>
-
-                  <div className="modal fade" tabIndex="-1" ref={parseExceptionModal} onChange={modalChangerHome}>
-                    <div className="modal-dialog">
-                      <div className="modal-content" >
-                        <div className="modal-header">
-                          <h5 className="modal-title">Success</h5>
-                          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                          <p>User was correctly created.</p>
-                        </div>
-                        <div className="modal-footer">
-                          <button onClick={modalChangerHome} hidden={modalChangerHome} type="button" className="btn btn-outline-secondary">
-                            Great !
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
                 </form>
-
 
                 Already have an account? <a href='/login' className='mb-3'><strong>Log in</strong></a>
 
               </MDBCol>
-
               <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
                 <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp' fluid />
               </MDBCol>
-
             </MDBRow>
           </MDBCardBody>
         </MDBCard>
