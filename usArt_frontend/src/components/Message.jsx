66d011830d80fc9fe3cbbcb6bb5e3f67e88c03ff
Marcus Vivar <marcospalominovivar@gmail.com>
2022-11-26 22:15:44 +0100

    message design

--- usArt_frontend/src/components/Message.jsx
+++ usArt_frontend/src/components/Message.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
         <div className="messageInfo">
             
             <div className="messageContent">
                 <p>Hello!</p>
-                <img src="https://thiscatdoesnotexist.com/" alt="" />
+                <img classname="im" src="https://thiscatdoesnotexist.com/" alt="" style={{height:"40px", width:"40px", borderRadius:"50%", objectFit:"cover"}} />
             </div>
         </div>
     </div>
