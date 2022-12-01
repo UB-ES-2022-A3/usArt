d780cb8e61982416b122b243c596fce8101aa8e2
Marcus Vivar <marcospalominovivar@gmail.com>
2022-12-01 19:53:45 +0100

    solved problem

--- usArt_frontend/src/components/profile.jsx
+++ usArt_frontend/src/components/profile.jsx
@@ -34,7 +34,7 @@ function Profile() {
                         <div className="ms-4 mt-5 d-flex flex-column" style={{ width: "150px" }}>
                             <img src={prof.photo}
                                 alt="Generic placeholder image" className="img-fluid img-thumbnail mt-4 mb-2"
-                                style={{ width: "150px", zIndex: "1" }} />
+                                style={{ minWidth: "150px", minHeight:"150px", maxWidth: "150px", maxHeight:"150px",zIndex: "1" }} />
 
                         </div>
                         <div className="ms-3" style={{ marginTop: "130px" }}>
