72ab9f21459ca26d56a7f34bf2891044562780fc
Ivan Sierra <isierrmo7@alumnes.ub.edu>
2022-12-06 17:32:12 +0100

    Endpoint per eliminar una publicació fet

--- usArt_backend/catalog/urls.py
+++ usArt_backend/catalog/urls.py
@@ -8,6 +8,7 @@ app_name = 'catalog'
 urlpatterns = [
     path('', views.PublicationList.as_view(), name='publications_list'),
     path('<str:pk>', views.PublicationDetail.as_view(), name='publication_details'),
+    path('delete/<str:pub_id>', views.PublicationDelete.as_view(), name='publication_delete'),
     path('user/<str:username>', views.PublicationUser.as_view(), name='publications_user'),
 
     path('user/commissions/<str:pub_id>', views.CommissionList.as_view(), name='commissions_user'),
@@ -16,4 +17,5 @@ urlpatterns = [
          name='commission_update_delete'),
     path('user/commission/post/', views.CommissionPost.as_view(), name='commission_post'),
     path('manage/post/', views.PublicationPosting.as_view(), name='post_publication')
+
 ]
