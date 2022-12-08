38476be5ab86a3fc05c3ab766e23b663cdd4d3f6
guillesmar <guillesmar3@gmail.com>
2022-12-08 17:00:24 +0100

    Combinació de les dues brancques UR-2-1 i UR-2-2

--- usArt_frontend/src/components/purchaseDetails.jsx
+++ usArt_frontend/src/components/purchaseDetails.jsx
@@ -90,7 +90,7 @@ function Details(props) {
     }
 
     function postComplain(description) {
-        //
+        // Aqui hem de enviar el correu 
         alert("Your complain and devolution has been sent")
     }
 
