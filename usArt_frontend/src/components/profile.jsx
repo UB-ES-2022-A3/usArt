7ce71f27084fa250b8375061564dbff28f57bf42
rogeranyor <rogeranyor@gmail.com>
2022-12-13 20:59:20 +0100

    Bug fix, images on upload art coudnt be deleted

--- usArt_frontend/src/components/profile.jsx
+++ usArt_frontend/src/components/profile.jsx
@@ -8,7 +8,6 @@ import Footer from './footer'
 import empty from '../assets/suchEmpty.png'
 import LINK_FRONTEND from './LINK_FRONTEND';
 import { Modal } from 'bootstrap'
-import { BsFillTrashFill } from "react-icons/bs";
 
 function Profile() {
 
@@ -575,11 +574,10 @@ function Profile() {
         }
     }
     const handleClearClick = (e) => {
-
         let fileReader = stateImages.filter(function (value, index, arr) {
-
-            return value.target !== e.target.src & e.target.accessKey !== index;
+            return value.target !== e.src & e.accessKey !== index;
         });
+        console.log(fileReader)
         setStateImages(fileReader)
 
     };
@@ -588,8 +586,8 @@ function Profile() {
         if (stateImages.length === 0) return
         return (
             <div class="image-div">
-                <img key={key} accessKey={key} onClick={handleClearClick} style={{ margin: "5px", borderRadius: "20px" }} src={images.target} className="size-img stack-images" alt="Img selected"></img>
-                <div class="trashContainer hidden_img">
+                <img key={key} id={"image"+key} accessKey={key} style={{ margin: "5px", borderRadius: "20px" }} src={images.target} className="size-img stack-images" alt="Img selected"></img>
+                <div onClick={()=>handleClearClick(document.getElementById("image"+key))} class="trashContainer hidden_img">
                     <div class="trash">
                         <div class="tap">
                             <div class="tip"></div>
@@ -604,9 +602,6 @@ function Profile() {
                         </div>
                     </div>
                 </div>
-
-
-
             </div>
         )
     }
